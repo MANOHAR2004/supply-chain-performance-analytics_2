b4cee3bd95dcbb9ea924c9c4d4c3c228f71bdffe
Manohar choudhary <choudharydav037@gmail.com>
2026-07-27 15:50:24 +0530

    mysql load file

--- sql/02_cleaning_2.sql
+++ sql/02_cleaning_2.sql
@@ -1,5 +1,14 @@
 USE supply_chain_db;
 
-
 select count(*) from scms_shipments;
-
+SELECT 
+    ID,
+    Country,
+    Vendor,
+    Shipment_Mode,
+    Scheduled_Delivery_Date,
+    Delivered_to_Client_Date,
+    Delivery_Delay_Days,
+    Delivery_Status
+FROM scms_shipments
+LIMIT 10;
