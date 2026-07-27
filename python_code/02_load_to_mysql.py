bb128699e3e5a2ef9d8293fc6cdccebb15628dc1
Manohar choudhary <choudharydav037@gmail.com>
2026-07-27 17:10:08 +0530

    change 02_load_to_mysql.py file

--- python_code/02_load_to_mysql.py
+++ python_code/02_load_to_mysql.py
@@ -6,11 +6,16 @@ import numpy as np
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Your_mysql_password",  # type you mysql password
+    password="Your_mysql_password",  # Enter your mysql password
     database="supply_chain_db"
 )
 cursor = conn.cursor()
 print("Connected successfully")
+
+# Truncate first
+cursor.execute("TRUNCATE TABLE scms_shipments")
+conn.commit()
+print("Table truncated")
 
 # Load cleaned data
 df = pd.read_csv(
@@ -22,13 +27,25 @@ df = pd.read_csv(
 df = df.replace({np.nan: None, "nan": None, "NaN": None, "None": None})
 print("Rows to insert:", len(df))
 
-# INSERT query — 38 placeholders
+# INSERT query
 insert_query = """
-    INSERT INTO scms_shipments VALUES (
+    INSERT INTO scms_shipments (
+        ID, Project_Code, PQ_Number, PO_SO_Number, ASN_DN_Number,
+        Country, Managed_By, Fulfill_Via, Vendor_INCO_Term, Shipment_Mode,
+        PQ_First_Sent_to_Client, PO_Sent_to_Vendor, Scheduled_Delivery_Date,
+        Delivered_to_Client_Date, Delivery_Recorded_Date, Product_Group,
+        Sub_Classification, Vendor, Item_Description, Molecule_Test_Type,
+        Brand, Dosage, Dosage_Form, Unit_of_Measure_Per_Pack,
+        Line_Item_Quantity, Line_Item_Value, Pack_Price, Unit_Price,
+        Manufacturing_Site, First_Line_Designation, Weight_Kilograms,
+        Freight_Cost_USD, Line_Item_Insurance_USD, Freight_Type,
+        Weight_Type, Delivery_Delay_Days,
+On_Time_Delivery, Delivery_Status, Dosage_Applicable
+    ) VALUES (
         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
         %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
-        %s, %s, %s, %s, %s, %s, %s, %s,%s
+        %s, %s, %s, %s, %s, %s, %s, %s, %s
     )
 """
 
